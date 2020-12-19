1e3eb9e12e58a2852b894215a5550ed48cb25562
John Ngala <oxymusicapp@gmail.com>
2020-12-19 11:22:33 +0300

    initial commit

--- server.js
+++ server.js
@@ -28,8 +28,9 @@ if(process.env.NODE_ENV === 'production'){
     //set static folder
     app.use(express.static('client/build'));
 
+    //index.html for all page routes
     app.get('*' , (req ,res) => {
-        res.sendFile(path.resolve(__dirname , 'client' , 'build' , 'index.html'));
+        res.sendFile(path.resolve(__dirname , '../client' , 'build' , 'index.html'));
     });
 }
 
