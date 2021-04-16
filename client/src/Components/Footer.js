bc90287809c63cf32eb2c951528232886324b26e
johnngala <johnngala5150@gmail.com>
2021-04-16 10:10:52 +0300

    inital commit

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -9,8 +9,7 @@ class Footer extends Component {
                 <a href ="https://twitter.com/_john_ngala"><img src = "images/twitter.png"  alt = "Twitter" className = "social-icons"/></a>
                 <a href ="https://www.instagram.com/_john_ng.a.la/"><img src = "images/instagram.png" alt = "Instagram" className = "social-icons" /></a>
                 <a href ="https://wa.me/254722882815"><img src = "images/whatsapp.png" alt = "Whatsapp" className = "social-icons" /></a>
-                
-                <h3 className = "developer">@john ngala 2020</h3>
+                <h3 className = "developer">@john ngala 2021</h3>
             </div>
         )
     }
